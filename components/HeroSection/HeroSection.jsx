03622b9504c1da8caebdefdede58646908b3c38d
tzafff <christzaf99@gmail.com>
2024-07-29 21:30:51 +0300

    Add HeroSection.jsx

---
+++ components/HeroSection/HeroSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from "next/image";
+
+import Style from './HeroSection.module.css'
+import {Button} from '../index'
+import images from '../../img'
+
+
+const HeroSection = () => {
+    return (
+        <div className={Style.heroSection}>
+            <div className={Style.heroSection_box}>
+                <div className={Style.heroSection_box_left}>
+                    <h1>Discover, collect, and sell NFTs 🖼️</h1>
+                    <p>
+                        Discover the most outstanding NFTs in all topics
+                        and sell them
+                    </p>
+                    <Button btnName="Start your Search"/>
+                </div>
+                <div className={Style.heroSection_box_right}>
+                    <Image src={images.hero} alt="hero section" width={600} height={600} />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HeroSection
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import React from "react";
-
+import Style from '../styles/index.module.css'
+import {HeroSection} from '../components/index'
 const index = () => {
-  return <div></div>;
+  return <div className={Style.homePage}>
+    <HeroSection />
+  </div>;
 };
 
 export default index;
