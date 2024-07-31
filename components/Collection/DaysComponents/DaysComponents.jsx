7363214f0a65de04d096ef2ca4d9dccdd0d13369
tzafff <christzaf99@gmail.com>
2024-07-31 13:12:40 +0300

    Add DaysComponents.jsx

--- components/Collection/DaysComponents/DaysComponents.jsx
+++ components/Collection/DaysComponents/DaysComponents.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
-
+import Image from "next/image";
+import {MdVerified} from "react-icons/md";
 import Style from './DaysComponents.module.css'
+import images from '../../../img'
 const DaysComponents = () => {
     return (
-        <div>DaysComponents</div>
+        <div className={Style.daysComponent}>
+            <div className={Style.daysComponent_box}>
+                <div className={Style.daysComponent_box_img}>
+                    <Image
+                        src={images.creatorbackground1}
+                        className={Style.daysComponent_box_img_img}
+                        alt="profile background"
+                        width={500}
+                        height={300}
+                        objectFit={"cover"}
+                    />
+                </div>
+
+                <div className={Style.daysComponent_box_profile}>
+                    <Image
+                        src={images.creatorbackground2}
+                        alt="profile"
+                        width={200}
+                        height={200}
+                        className={Style.daysComponent_box_img_1}
+                        objectFit={"cover"}
+                    />
+                    <Image
+                        src={images.creatorbackground4}
+                        alt="profile"
+                        width={200}
+                        height={200}
+                        className={Style.daysComponent_box_img_2}
+                        objectFit={"cover"}
+                    />
+                    <Image
+                        src={images.creatorbackground3}
+                        alt="profile"
+                        width={200}
+                        height={200}
+                        className={Style.daysComponent_box_img_3}
+                        objectFit={"cover"}
+                    />
+                </div>
+
+                <div className={Style.daysComponent_box_title}>
+                    <h2>Amazing Collection</h2>
+                    <div className={Style.daysComponent_box_title_info}>
+                        <div className={Style.daysComponent_box_title_info_profile}>
+                            <Image
+                                src={images.user1}
+                                alt="profile"
+                                width={30}
+                                height={30}
+                                objectFit={"cover"}
+                                className={Style.daysComponent_box_title_info_profile_img}
+                            />
+
+                            <p>
+                                Creator
+                                <span>
+                                    Chris Tzaf
+                                    <small>
+                                        <MdVerified/>
+                                    </small>
+                                </span>
+                            </p>
+                        </div>
+
+                        <div className={Style.daysComponent_box_title_info_price}>
+                            <small>1.255 ETH</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default DaysComponents
