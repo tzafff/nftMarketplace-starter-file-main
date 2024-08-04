40b443d8a5b0028033ffb85268acf388e600d8c0
tzafff <christzaf99@gmail.com>
2024-08-04 15:16:29 +0300

    Add Collection Page

---
+++ pages/collection.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Style from '../styles/collection.module.css'
+import images from '../img'
+import {Banner, CollectionProfile, NFTCardTwo} from '../collectionPage/index'
+import {Slider, Brand} from '../components/index'
+import Filter from '../components/Filter/Filter'
+const Collection = () => {
+
+    const collectionArray = [
+        images.nft_image_1,
+        images.nft_image_2,
+        images.nft_image_3,
+        images.nft_image_2,
+        images.nft_image_3,
+        images.nft_image_1,
+        images.nft_image_1,
+        images.nft_image_3,
+        images.nft_image_2,
+    ]
+
+    return (
+        <div className={Style.collection}>
+            <Banner bannerImage={images.creatorbackground1} />
+            <CollectionProfile NFTData={collectionArray}/>
+            <Filter />
+            <NFTCardTwo NFTData={collectionArray} />
+            <Slider />
+            <Brand />
+        </div>
+    )
+}
+export default Collection
