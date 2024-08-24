2d9fc3fbc2b43620f72d4bfaf0d4a5eedceaf56a
tzafff <christzaf99@gmail.com>
2024-08-24 21:41:55 +0300

    Navigate to nft details page

--- components/NFTCard/NFTCard.jsx
+++ components/NFTCard/NFTCard.jsx
@@ -4,6 +4,7 @@ import {BsImages} from "react-icons/bs";
 import Image from "next/image";
 import images from '../../img'
 import Style from './NFTCard.module.css'
+import Link from 'next/link'
 const NftCard = ({NFTData}) => {
 
 
@@ -32,63 +33,65 @@ const NftCard = ({NFTData}) => {
     return (
         <div className={Style.NFTCard}>
             {NFTData.map((el, i) => (
-                <div className={Style.NFTCard_box} key={i+1}>
-                    <div className={Style.NFTCard_box_img}>
-                        <Image
-                            src={el.image}
-                            alt="NFT IMAGE"
-                            width={600}
-                            height={600}
-                            className={Style.NFTCard_box_img_img}
-                        />
-                    </div>
-
-                    <div className={Style.NFTCard_box_update}>
-                        <div className={Style.NFTCard_box_update_left}>
-                            <div
-                                className={Style.NFTCard_box_update_left_like}
-                                onClick={() => likeNft()}
-                            >
-                                {like ? (
-                                    <AiOutlineHeart />
-                                ) : (
-                                    <AiFillHeart
-                                        className={Style.NFTCard_box_update_left_like_icon}
-                                    />
-                                )}
-                                {" "} 22
-                            </div>
+                <Link href={{pathname:"/nft-details", query: el}} >
+                    <div className={Style.NFTCard_box} key={i+1}>
+                        <div className={Style.NFTCard_box_img}>
+                            <Image
+                                src={el.image}
+                                alt="NFT IMAGE"
+                                width={600}
+                                height={600}
+                                className={Style.NFTCard_box_img_img}
+                            />
                         </div>
 
-                        <div className={Style.NFTCard_box_update_right}>
-                            <div className={Style.NFTCard_box_update_right_info}>
-                                <small>Remaining Time</small>
-                                <p>3h : 15m: 20s</p>
+                        <div className={Style.NFTCard_box_update}>
+                            <div className={Style.NFTCard_box_update_left}>
+                                <div
+                                    className={Style.NFTCard_box_update_left_like}
+                                    onClick={() => likeNft()}
+                                >
+                                    {like ? (
+                                        <AiOutlineHeart />
+                                    ) : (
+                                        <AiFillHeart
+                                            className={Style.NFTCard_box_update_left_like_icon}
+                                        />
+                                    )}
+                                    {" "} 22
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className={Style.NFTCard_box_update_details}>
-                        <div className={Style.NFTCard_box_update_details_price}>
-                            <div className={Style.NFTCard_box_update_details_price_box}>
-                                <h4>{el.name} #{el.tokenId}</h4>
 
-                                <div className={Style.NFTCard_box_update_details_price_box_box}>
-                                    <div className={Style.NFTCard_box_update_details_price_box_bid}>
-                                        <small>Current Bid</small>
-                                        <p>{el.price}ETH</p>
-                                    </div>
-                                    <div className={Style.NFTCard_box_update_details_price_box_stock}>
-                                        <small>63 in stock</small>
-                                    </div>
+                            <div className={Style.NFTCard_box_update_right}>
+                                <div className={Style.NFTCard_box_update_right_info}>
+                                    <small>Remaining Time</small>
+                                    <p>3h : 15m: 20s</p>
                                 </div>
                             </div>
                         </div>
+                        <div className={Style.NFTCard_box_update_details}>
+                            <div className={Style.NFTCard_box_update_details_price}>
+                                <div className={Style.NFTCard_box_update_details_price_box}>
+                                    <h4>{el.name} #{el.tokenId}</h4>
 
-                        <div className={Style.NFTCard_box_update_details_category}>
-                            <BsImages />
+                                    <div className={Style.NFTCard_box_update_details_price_box_box}>
+                                        <div className={Style.NFTCard_box_update_details_price_box_bid}>
+                                            <small>Current Bid</small>
+                                            <p>{el.price}ETH</p>
+                                        </div>
+                                        <div className={Style.NFTCard_box_update_details_price_box_stock}>
+                                            <small>63 in stock</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={Style.NFTCard_box_update_details_category}>
+                                <BsImages />
+                            </div>
                         </div>
                     </div>
-                </div>
+                </Link>
             ))}
         </div>
     )
