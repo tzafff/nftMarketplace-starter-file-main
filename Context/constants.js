30502c3f0b68f19d84ac88b9cbe9d62472becb47
tzafff <christzaf99@gmail.com>
2024-08-26 23:05:42 +0300

    Add Sepolia contract.

--- Context/constants.js
+++ Context/constants.js
@@ -1,4 +1,4 @@
 import nftMarketplace from '../artifacts/contracts/NftMarketPlace.sol/NftMarketPlace.json'
 
-export const NFTMarketplaceAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const NFTMarketplaceAddress = "0xF18a3E2BDB54E900c6F63c91c304b4a790052364"
 export const NFTMarketplaceABI = nftMarketplace.abi;
