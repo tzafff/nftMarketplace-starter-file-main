b96b9d5049a88335d7e2dbc684b1351a37b15a7c
tzafff <christzaf99@gmail.com>
2024-08-26 21:24:11 +0300

    Remove images

--- img/index.js
+++ img/index.js
@@ -10,32 +10,13 @@ import creatorbackground8 from "./creatorbackground-8.jpg";
 import creatorbackground9 from "./creatorbackground-9.jpg";
 import creatorbackground10 from "./creatorbackground-10.jpg";
 import creatorbackground11 from "./creatorbackground-11.jpg";
-import user1 from "./user-1.png";
-import user2 from "./user-2.png";
-import user3 from "./user-3.png";
-import user4 from "./user-4.png";
-import user5 from "./user-5.png";
-import user6 from "./user-6.png";
-import user7 from "./user-7.png";
-import user8 from "./user-8.png";
-import user9 from "./user-9.png";
-import user10 from "./user-10.png";
 import service1 from "./service-1.png";
 import service2 from "./service-2.png";
 import service3 from "./service-3.png";
 import service4 from "./service-4.png";
-import logoLight from "./logo-light.svg";
-import logo from "./logo.svg";
-import facebook from "./facebook.svg";
-import twitter from "./twitter.svg";
-import telegram from "./telegram.svg";
-import youtube from "./youtube.svg";
-import vimeo from "./vimeo.svg";
-import vector from "./VectorHIW.svg";
 import zgcmq from "./zgcmq.svg";
 import hero from "./hero.png";
 import nft_1 from "./nft_1.png";
-import getAble from "./getable.png";
 import nft_image_1 from "./nft-image-1.png";
 import nft_image_2 from "./nft-image-2.png";
 import nft_image_3 from "./nft-image-3.png";
@@ -44,8 +25,6 @@ import update from "./update.png";
 import earn from './eran.png'
 import upload from './upload2.png'
 import metamask from './metamask.png'
-import walletconnect from './walletconnect.png'
-import coinbase from './coibase.webp'
 import loader from './loader.gif'
 import error2 from './error2.png'
 import rap7 from './7.png'
@@ -77,8 +56,6 @@ export default {
   rapper5,
   rapper6,
   loader,
-  coinbase,
-  walletconnect,
   metamask,
   upload,
   earn,
@@ -88,7 +65,6 @@ export default {
   nft_image_1,
   nft_image_2,
   nft_image_3,
-  getAble,
   nft_1,
   hero,
   creatorbackground1,
@@ -102,27 +78,9 @@ export default {
   creatorbackground9,
   creatorbackground10,
   creatorbackground11,
-  user1,
-  user2,
-  user3,
-  user4,
-  user5,
-  user6,
-  user7,
-  user8,
-  user9,
-  user10,
   service1,
   service2,
   service3,
   service4,
-  logo,
-  logoLight,
-  facebook,
-  telegram,
-  twitter,
-  vector,
-  vimeo,
-  youtube,
   zgcmq,
 };
