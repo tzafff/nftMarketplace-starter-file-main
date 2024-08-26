db90836d6366296a503cdc6def160ebc451e0580
tzafff <christzaf99@gmail.com>
2024-08-26 23:11:06 +0300

    Fix path

--- Context/constants.js
+++ Context/constants.js
@@ -1,4 +1,4 @@
-import nftMarketplace from '../artifacts/contracts/NftMarketPlace.sol/NftMarketPlace.json'
+import nftMarketplace from '../Context/NftMarketPlace.json'
 
 export const NFTMarketplaceAddress = "0xF18a3E2BDB54E900c6F63c91c304b4a790052364"
 export const NFTMarketplaceABI = nftMarketplace.abi;
